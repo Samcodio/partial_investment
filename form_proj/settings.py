4a179667f689847b8104ac1d9c4632cf90e66c6e
Samcodio <nosikesamuel1@gmail.com>
2024-06-09 10:34:38 -0700

    debug false

--- form_proj/settings.py
+++ form_proj/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w8=!_@ouo2wjn%5vuduuad(c*jq$tun2t@5^t9&if#ktbd^$0_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
